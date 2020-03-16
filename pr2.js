05075e2daddf2a61724e8d6514fd6da13a2f4aa7
pogikeith <pogikeith@yahoo.com>
2020-03-16 16:55:39 -0600

    strings

--- pr2.js
+++ pr2.js
@@ -1,2 +1,3 @@
-document.write ('I love using js and and using external files')
-
+document.write ('<strong>I love using js</strong> and and using external files\n');
+document.write ("<strong>js rules! </strong>\nthis is fun\n");
+document.write ("john said \"javascript is easy\"");
