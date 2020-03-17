1a20e79951b86ae013e7ef06ed329b8d805d48f4
pogikeith <pogikeith@yahoo.com>
2020-03-17 16:37:26 -0600

    ch3

---
+++ ch3.js
@@ -0,0 +1,6 @@
+document.write('<h1>A Page of JS</h1>');
+var headingtext = '<h1>A Page of JS</h1>';
+document.write(headingtext);
+var myintro = 'Hello Welcome to my JS Page'; 
+var linktag = '<a href=\'http://scripttheweb.com\'>Link to Site</a>';
+var redtext = 'span style=\'color:red\'>I am so colorful today!</span>'; 
