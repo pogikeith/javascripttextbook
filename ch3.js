936264acd1bab1cc065476c33c4336bca199394e
pogikeith <pogikeith@yahoo.com>
2020-03-17 18:14:27 -0600

    changes

--- ch3.js
+++ ch3.js
@@ -1,6 +1,20 @@
-document.write('<h1>A Page of JS</h1>');
+// document.write('<h1>A Page of JS</h1>');
 var headingtext = '<h1>A Page of JS</h1>';
-document.write(headingtext);
 var myintro = 'Hello Welcome to my JS Page'; 
 var linktag = '<a href=\'http://scripttheweb.com\'>Link to Site</a>';
-var redtext = 'span style=\'color:red\'>I am so colorful today!</span>'; 
+var redtext = '<span style=\'color:red\'>I am so colorful today!</span>';
+
+var begineffect = '<strong>';
+var endeffect = '</strong>'; 
+var beginpara= '<p>';
+var endpara = '</p>'; 
+
+document.write(headingtext);
+document.write(myintro);
+document.write(linktag);
+document.write(redtext);
+
+document.write(headingtext);
+document.write(begineffect  + myintro + endeffect);
+document.write(beginpara  + linktag + endpara);
+document.write(beginpara  + redtext + endpara);
