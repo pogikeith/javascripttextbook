2a1274d7380e26cde5cb3a7f4b4c93848ad0f54e
pogikeith <pogikeith@yahoo.com>
2020-03-23 18:39:42 -0600

    functs

---
+++ Ch4/func.js
@@ -0,0 +1,16 @@
+
+function reallycool (coolcar, coolplace) {
+  
+    document.write('my car is a ' + coolcar + ' and i have to ' + coolplace); 
+}
+reallycool()
+
+
+function getaddedtxt() {
+    var textpart1 = "this is a ";
+    var textpart2 = "funday";
+    var addedtext = textpart1 + textpart2; 
+    return addedtext;  
+}
+getaddedtxt()
+ 
