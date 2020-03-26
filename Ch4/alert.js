992f265693885b775e9b5562a0451cece1fcdfc6
pogikeith <pogikeith@yahoo.com>
2020-03-25 21:39:09 -0600

    globalvar

--- Ch4/alert.js
+++ Ch4/alert.js
@@ -1,6 +1,26 @@
-function show_message() {
-    window.alert('this is an alert');
+// function show_message() {
+//     window.alert('this is an alert');
+// }
+// window.alert('i am first');
+// window.alert('am second')
+// show_message();
+
+// var mycar = "honda"; 
+// var paycheck = '1200'; 
+
+// function new_car () {
+//     var  mycar = 'ferrari';
+//     var  paycheck = '500'
+//     window.alert('you need $' + paycheck + ' to get a ' + mycar)
+// }
+// new_car(); 
+// window.alert('you make $' + paycheck + ' and have a ' + mycar)
+
+
+
+function new_car () {
+    var  mycar = 'ferrari';
+    var  paycheck = '500'
+    window.alert('you need $' + paycheck + ' to get a ' + mycar)
 }
-window.alert('i am first');
-window.alert('am second')
-show_message();
+new_car(); 
