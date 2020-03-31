563b7a9f2a2f72a0699954e8aea83a92269a6c82
pogikeith <pogikeith@yahoo.com>
2020-03-30 22:44:19 -0600

    globalvar

--- Ch4/alert.js
+++ Ch4/alert.js
@@ -18,9 +18,45 @@
 
 
 
-function new_car () {
-    var  mycar = 'ferrari';
-    var  paycheck = '500'
-    window.alert('you need $' + paycheck + ' to get a ' + mycar)
-}
-new_car(); 
+// function new_car () {
+//     var  mycar = 'ferrari';
+//     var  paycheck = '500'
+//     window.alert('you need $' + paycheck + ' to get a ' + mycar)
+// }
+// new_car(); 
+
+
+// function check_alert(pcheck) {
+//     window.alert('you make $' + pcheck);
+
+// }
+
+// var paycheck = 1200;
+// check_alert(paycheck); 
+
+
+// function check_alert(pcheck, car) {
+//     window.alert('you make $' + pcheck + ' and have a ' + car);
+// }
+
+// check_alert(1200, 'corvette'); 
+// check_alert(500+800, 'corv'+'ette');
+
+// function get_added_text () {
+//     var textpart1 = 'This is ';
+//     var textpart2 = 'fun ' ; 
+//     var addextext = textpart1 + textpart2; 
+//     return addextext; 
+// }
+// var alerttext = get_added_text ();
+// window.alert(alerttext); 
+
+// function do_not_click () {
+//     window.alert ('do not click on my page!')
+
+// }
+// document.onclick = do_not_click;
+
+document.onclick = function () {
+    window.alert('dont click'); 
+}; 
---
+++ Ch4/prjs4.js
@@ -0,0 +1,28 @@
+// function car_cost(mycar, paycheck) {
+//     window.alert('You have a ' + mycar + ' and make ' + paycheck)
+// }
+
+
+// function get_added_text() {
+//     var text1 = ('this project ');
+//     var text2 = ('is almost fun');  
+//     var added_text = text1+text2; 
+//     return(added_text); 
+// }
+
+// car_cost('Mustang',1200); 
+// var alerttext = get_added_text();
+// window.alert(alerttext);
+
+var myname = 'john'
+function say_name () {
+    var mydog = 'joey'; 
+    function say_food() {
+        var myfood = 'pizza'; 
+        window.alert(myname + ' and ' + mydog + 'like ' + myfood); 
+    }
+    window.alert(myname + ' has a dog ' + mydog); 
+    say_food
+
+}
+say_name();
