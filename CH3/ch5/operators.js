a8b47a73f4d928c507f6de3a9c36e2ed5de84f5c
pogikeith <pogikeith@yahoo.com>
2020-04-02 16:35:06 -0600

    opers

---
+++ CH3/ch5/operators.js
@@ -0,0 +1,11 @@
+// let num1 = 11%2; 
+// window.alert(num1); 
+
+// let num1 = 2; 
+//     theresultl = ++num1;
+//     window.alert(num1);  
+
+
+let num1 = 2; 
+theresultl = num1++;
+window.alert(num1);
--- Ch4/prjs4_2.js
+++ Ch4/prjs4_2.js
@@ -1,1 +1,9 @@
+function display_HTML() {
+    let myheading = '<h1>Hello WOrld</h1>';
+    let mytext = `<p>while it was nice to know you worldthere are 
+        only some things I am comfortable with sharing in a global context
+        you cant alter the variable that holds this text outside of the block.
+    </p>`; 
+    document.write(myheading + mytext);
+}
 display_HTML(); 
