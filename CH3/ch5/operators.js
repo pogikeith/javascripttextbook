fecc57449be913e7f8b74a7a97d11f76d7afc83b
pogikeith <pogikeith@yahoo.com>
2020-04-06 21:50:08 -0600

    operand

--- CH3/ch5/operators.js
+++ CH3/ch5/operators.js
@@ -6,6 +6,74 @@
 //     window.alert(num1);  
 
 
-let num1 = 2; 
-theresultl = num1++;
-window.alert(num1);
+// let num1 = 2; 
+// theresultl = num1++;
+// window.alert(num1);
+
+
+
+// let num1 = 2; 
+//     result = ++num1;
+// alert('num1= ' + 'result=  ' + result);
+// num1 = 2; 
+// result = num1++;
+// alert('num1= ' + 'result=  ' + result )
+
+
+// let num1 = 2,
+//     theresult = --num1; 
+// alert(num1)
+
+
+// let num1 = 4,
+//     num2  = '7', 
+//     thesum = num1 + num2; 
+// window.alert(thesum); 
+
+
+// let num1 = 4,
+//     num2  = '7', 
+//     thesum = num1 + (+num2); 
+// window.alert(thesum); 
+
+
+// var square = 2**3
+// window.alert(square); 
+
+// x =  2 
+// window.alert(x *=3); 
+
+
+// let mymoney = 1000;
+// let bonus = 500;
+// mymoney +=bonus; 
+// window.alert(mymoney); 
+
+
+// let mymoney = 1000;
+// let bonus = 500;
+// mymoney -=bonus; 
+// window.alert(mymoney); 
+
+// let mymoney = 1000;
+// let bonus = 500;
+// mymoney *=bonus; 
+// window.alert(mymoney); 
+
+
+// let mymoney = 1000;
+// let bonus = 500;
+// mymoney /=bonus; 
+// window.alert(mymoney); 
+
+
+// let mymoney = 1000;
+// let bonus = 400;
+// mymoney %=bonus; 
+// window.alert(mymoney); 
+
+
+let mymoney = 1000;
+let bonus = 4;
+mymoney **=bonus; 
+window.alert(mymoney); 
---
+++ CH3/ch5/pr5.js
@@ -0,0 +1,7 @@
+var paycheck = 2000
+document.write(paycheck += 2000)  ;
+var paysubtract = 3500
+document.write(paycheck=-paysubtract);
+document.write(paycheck*=0)
+document.write(paycheck+=500)
+document.write(paycheck-=100)
