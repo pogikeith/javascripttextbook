d09b488f6bbc85c4f58872b7861c33999d4360da
pogikeith <pogikeith@yahoo.com>
2020-04-07 11:59:06 -0600

    proj

--- CH3/ch5/pr5.js
+++ CH3/ch5/pr5.js
@@ -1,7 +1,18 @@
-var paycheck = 2000
-document.write(paycheck += 2000)  ;
-var paysubtract = 3500
-document.write(paycheck=-paysubtract);
-document.write(paycheck*=0)
-document.write(paycheck+=500)
-document.write(paycheck-=100)
+// var paycheck = 2000
+// document.write(paycheck += 2000)  ;
+// var paysubtract = 3500
+// document.write(paycheck=-paysubtract);
+// document.write(paycheck*=0)
+// document.write(paycheck+=500)
+// document.write(paycheck-=100)
+
+let num1 = 2,
+    num2 = 3,
+    num3 = 3; 
+document.write(num3 != num2)
+
+
+let num1 = 2,
+    num2 = 3,
+    num3 = 3; 
+document.write(num3 != num2)
