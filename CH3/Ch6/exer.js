87855d313c977eb1d14a9e4b919313100b3c84b0
pogikeith <pogikeith@yahoo.com>
2020-04-09 12:00:40 -0600

    ch6

---
+++ CH3/Ch6/exer.js
@@ -0,0 +1,21 @@
+// let boats = 0; 
+// if (boats === 3) {
+//     window.alert('yes there are 3 boats');
+// }
+// else {
+//     window.alert('you dont have the right amount')
+// v
+
+let cookbook = 'yes',
+    meatloafrec = 'no';
+if (cookbook === 'yes') {
+    if (meatloafrec === 'yes') {
+    window.alert('recipe found');
+}
+    else {
+    window.alert('have the book but no recipe'); 
+    }
+}
+else {
+    window.alert('you need book'); 
+} 
---
+++ CH3/ch5/pr5_2.js
@@ -0,0 +1,8 @@
+let num1 = 9;
+let num2 = 7;
+if (num1 < num2) {
+    window.alert('true');
+}
+else {
+    window.alert('false')
+}
