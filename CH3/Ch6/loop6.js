27277252ea5f37536c20b9982aa0cfbcbf1474b9
pogikeith <pogikeith@yahoo.com>
2020-04-22 22:11:52 -0600

    loop

--- CH3/Ch6/loop6.js
+++ CH3/Ch6/loop6.js
@@ -1,10 +1,28 @@
-document.write('get ready for some repeated text.<br>');
-for (let count = 1; count < 11; count++) {
-    document.write(count + '. I am part of the loop. <br>'); 
-    for (let nestcount = 1; nestcount < 3; nestcount ++) {
+// document.write('get ready for some repeated text.<br>');
+// for (let count = 1; count < 11; count++) {
+//     document.write(count + '. I am part of the loop. <br>'); 
+//     for (let nestcount = 1; nestcount < 3; nestcount ++) {
 
-    }
+//     }
+// }
+// document.write('now we are back to plain<br>');
+
+// document.write('get ready for some repeated text.<br>');
+// for (let count = 1; count < 11; count++) {
+//     document.write(`the main count is ${count}`); 
+//     for (let nestcount = 1; nestcount < 3; nestcount ++) {
+
+//     }
+// }
+// document.write('now we are back to plain<br>');
+
+// for (let i = 25; i < 50; i++) {
+//     document.write('this can be stopped'); 
+
+// }
+
+
+for (let i = 25, j = 10;  i < 11; i++, j+=10) {
+    document.write(j + '<br>'); 
+
 }
-
-
-
