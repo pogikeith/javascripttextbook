13687500ca6c084f70cc0d6f158ad576734a1d63
pogikeith <pogikeith@yahoo.com>
2020-05-07 21:43:16 -0600

    loopa

--- CH3/Ch6/loop6.js
+++ CH3/Ch6/loop6.js
@@ -53,4 +53,16 @@ for (let num =1; num < 11; num ++) {
 else {
     document.write(num + 'i am part of a loop<br>');
   }
+
+
+
+let stopnumber = 5; 
+for (let num =1; num < 11; num ++) {
+    if (num === stopnumber) {
+        document.write('sorry loop stops here<br>')
+    }
+
+else {
+    document.write(num + 'i am part of a loop<br>');
+  }
 }
