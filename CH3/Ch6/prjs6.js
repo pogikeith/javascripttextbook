c4b31f73bca4895157787211fb0abe87896d19d2
pogikeith <pogikeith@yahoo.com>
2020-05-13 22:07:22 -0600

    prjs6

--- CH3/Ch6/prjs6.js
+++ CH3/Ch6/prjs6.js
@@ -1,21 +1,37 @@
-let msg = ""
-    city = window.prompt('what is the name of your city?')
-if ( (city = null) || (city = '') ); {
-document.write('no city entered')
+let items = 0; 
+    msg = ""
+    city = window.prompt('what is the name of your city?', ''); 
+if ( (city === null) || (city === '') ) {
+    msg = 'no city entered';
 }
 else {
         switch (city) {
             case 'johsontown' :
                  msg = 'delivery in 3 days';
+                 items = 10;
+         
         break;
             case 'donville':
                 msg = 'delivery 1 week'
+                items = 5
+
         break;
             case 'danieltown': //fallthrough
             case 'marysville' : 
                  msg = '2 weeks' 
+                 items = 2
         break; 
             default : 'sorry no delivery'
+            items = 0
     }
 }
 document.write(msg);
+if (items > 0) {
+    document.write(
+        '<p> We canb deliver up to ' + items +
+        ' and mail to order</p>'
+    );
+    for (let count = 1; count <= items; count ++) {
+        document.write(count + '. _________<br>'); 
+    }
+}
