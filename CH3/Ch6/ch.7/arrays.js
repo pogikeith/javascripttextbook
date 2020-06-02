77062d337ee95d960487fe0e1ac5e0cf239ba4b6
pogikeith <pogikeith@yahoo.com>
2020-06-02 08:30:05 -0600

    morearrays

--- CH3/Ch6/ch.7/arrays.js
+++ CH3/Ch6/ch.7/arrays.js
@@ -1,3 +1,17 @@
 // let arrayname = newarray();
-let students = new Array('thomas', 'roger', 'sarah', 'mike');
-document.write('<p>My Students</p>')
+// let students = new Array('thomas', 'roger', 'sarah', 'mike');
+// document.write('<p>My Students</p>');
+// document.write(students[0] + '<br>');
+// document.write(students[1] + '<br>');
+// document.write(students[2] + '<br>');
+// document.write(students[3] + '<br>');
+
+
+// let students = ['thomas', 'roger', 'sarah', 'mike'];
+// alert(students.length)
+
+let students = new Array('thomas', 'roger', 'sarah', 'mike', 'keith', 'ash');
+document.write('<p>My Students</p>');
+for (let i = 0; i < students.length; i++) {
+    document.write(students[i] + '<br>'); 
+}
