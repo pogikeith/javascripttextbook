96b898ead4e00160b458797de7df9137f35d3c17
pogikeith <pogikeith@yahoo.com>
2020-06-03 19:38:11 -0600

    more

---
+++ ch.7/prjs7.js
@@ -0,0 +1,4 @@
+let computer_parts = ['monitors' , 'motherboard', 'chips', 'hard drives', 'dvdroms', 'cases', 'power supplies']
+for (let i = 0; i < computer_parts.length; i++) {
+    document.write(computer_parts[i] + "<br>")
+}
